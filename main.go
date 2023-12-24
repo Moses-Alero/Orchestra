162a3565259a09911bb1a45474a23cc261529970
Moses-Alero <mosesalero@gmail.com>
2023-12-24 14:12:46 +0100

    intonare

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+
+)
+
+
+//My humble implementation of a container orchestration system. 
+//This is purely for learning purposes but if I make something others can use
+//that would certainly be extra cool
+
+func main(){
+  fmt.Printf("Hello! Welcome to Orchestra, This is the beginning of your journey :)")
+}
